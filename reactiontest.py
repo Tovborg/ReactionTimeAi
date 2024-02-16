96f5d30fe5e40ad337c8cc6e17d289b624bce85b
Emil Tovborg-Jensen <emiltovborg-jensen@MacBook-Pro-tilhrende-Emil.local>
2024-02-16 23:58:41 +0100

    Isolated environment into class

--- reactiontest.py
+++ reactiontest.py
@@ -1,18 +1,26 @@
 import time
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
 
-driver.get("https://humanbenchmark.com/tests/reactiontime")
+class ReactionTimeEnvironment:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
 
-# Await cookies popup
-time.sleep(3)
+        self.driver.get("https://humanbenchmark.com/tests/reactiontime")
 
-cookies_accept = driver.find_element(By.CSS_SELECTOR, "button.css-47sehv")
-cookies_accept.click()
+        # Await cookies popup
+        time.sleep(3)
+
+        cookies_accept = self.driver.find_element(By.CSS_SELECTOR, "button.css-47sehv")
+        cookies_accept.click()
+
+    def reset(self):
+        self.driver.refresh()
 
 
+reaction_time_env = ReactionTimeEnvironment()
 
 input("Press Enter to quit...")
 
