a37aa22c7feebd49b50e8229e3f3b73d17bfcd02
Emil Tovborg-Jensen <emiltovborg-jensen@MacBook-Pro-tilhrende-Emil.local>
2024-02-17 18:13:31 +0100

    initial implementation of ReactionTimeEnvironment

--- reactiontest.py
+++ reactiontest.py
@@ -8,7 +8,8 @@ from selenium.webdriver.common.by import By
 class ReactionTimeEnvironment:
     def __init__(self):
         self.driver = webdriver.Chrome()
-        self.reaction_time = None
+        # Initialize at extremely high value to ensure that the first reaction time is always better
+        self.best_reaction_time = 1000000000
         self.game_started = False
 
         self.driver.get("https://humanbenchmark.com/tests/reactiontime")
@@ -28,24 +29,39 @@ class ReactionTimeEnvironment:
     # Environment state methods
     def get_state(self):
         square_color = self.get_square_color()
-        # check for view-splash class
+
+        # Check for different states
+        game_started = False
+        game_finished = False
+        early_click = False
+
         try:
-            view_splash = self.driver.find_element(By.CSS_SELECTOR, "div.view-splash")
+            self.driver.find_element(By.CSS_SELECTOR, "div.view-splash")
             game_started = False
         except selenium.common.exceptions.NoSuchElementException:
             game_started = True
+
         try:
-            game_over = self.driver.find_element(By.CSS_SELECTOR, "div.view-result")
-            game_over = True
+            self.driver.find_element(By.CSS_SELECTOR, "div.view-result")
+            game_finished = True
+            self.reset()
         except selenium.common.exceptions.NoSuchElementException:
-            game_over = False
+            pass  # Game over indicator not found, game is still ongoing
+
+        try:
+            self.driver.find_element(By.CSS_SELECTOR, "div.view-scold")
+            early_click = True
+            self.reset()
+        except selenium.common.exceptions.NoSuchElementException:
+            pass  # Early click indicator not found, click was not too early
 
         state = {
             "square_color": square_color,
             "game_started": game_started,
-            "game_over": game_over,
+            "game_finished": game_finished,
+            "early_click": early_click
         }
-
+        print(state)
         return state
 
     def get_square_color(self):
@@ -56,8 +72,9 @@ class ReactionTimeEnvironment:
     def get_reaction_time(self):
         reaction_time = self.driver.find_element(By.CSS_SELECTOR, "div.css-1qvtbrk.e19owgy78").text
         react_time_number = int(re.search(r'\d+', reaction_time).group())
-        self.reaction_time = react_time_number
+
         print(f"Your reaction time is {react_time_number}ms")
+        return react_time_number
 
     # Gym methods
     def step(self, action):
@@ -66,20 +83,31 @@ class ReactionTimeEnvironment:
 
         state = self.get_state()
 
-    def calculate_reward_and_done(self):
-        pass
+        reward, done = self.calculate_reward_and_done(state, action=action)
+        print(reward, done)
 
+    def calculate_reward_and_done(self, state, action):
+        reward = 0
+        done = False
 
+        if state['game_started'] is False and state['square_color'] == "#2b87d1" and action == "click":
+            reward += 100
+            done = False
+        if state['game_started'] and state['game_finished'] is False and action == "click" and state['square_color'] == "#4bdb6a":
+            reward += 100
+            done = True
 
+            agent_reaction_time = self.get_reaction_time()
+            if agent_reaction_time < self.best_reaction_time:
+                self.best_reaction_time = agent_reaction_time
+                reward += 150
+            else:
+                reward -= 150
+        if state['game_started'] and state['game_finished'] is False and action == "click" and state['early_click']:
+            reward -= 200
+            done = True
 
-reaction_time_env = ReactionTimeEnvironment()
-
-while True:
-    reaction_time_env.get_state()
-    if reaction_time_env.get_state()["game_over"]:
-        reaction_time_env.get_reaction_time()
-        reaction_time_env.reset()
-    time.sleep(0.2)
+        return reward, done
 
 
 
